449487fc955cd9f45feaeb90e896b4d069403c05
Ben Nortier <ben@bjnortier.com>
2014-11-14 10:58:55 +0100

    Fix test for meshing concave polygons

--- test/meshplanar.test.js
+++ test/meshplanar.test.js
@@ -30,9 +30,40 @@ describe('Mesh Planar', function() {
       {x: 1, y: 0, z: 0},
     ]);
     assert.triangleEqual(mesh[1], [
+      {x: 0, y: 1, z: 0},
       {x: 1, y: 0, z: 0},
       {x: 1, y: 1, z: 0},
+    ]);
+
+  });
+
+  it('can mesh a concave polygon in the XY plane', function() {
+
+    var polygon = [
+      new Vec3(0,0,0),
+      new Vec3(1,0,0),
+      new Vec3(0.5,0.5,0),
+      new Vec3(1,1,0),
+      new Vec3(0,1,0),
+    ];
+
+    var mesh = meshPlanar(polygon);
+    assert.equal(mesh.length, 3);
+
+    assert.triangleEqual(mesh[0], [
+      {x: 0, y: 0, z: 0},
+      {x: 1, y: 0, z: 0},
+      {x: 0.5, y: 0.5, z: 0},
+    ]);
+    assert.triangleEqual(mesh[1], [
       {x: 0, y: 1, z: 0},
+      {x: 0, y: 0, z: 0},
+      {x: 0.5, y: 0.5, z: 0},
+    ]);
+    assert.triangleEqual(mesh[2], [
+      {x: 0, y: 1, z: 0},
+      {x: 0.5, y: 0.5, z: 0},
+      {x: 1, y: 1, z: 0},
     ]);
 
   });
